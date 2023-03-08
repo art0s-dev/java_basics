cc12405985f741e95da38edf3da795796ab6d531
Till Kaiser <art0s@mailbox.org>
2023-03-08 20:24:09 +0100

    chore(CostCalculation): refactored the product cost calculation method to make it more readable

--- src/main/java/school/productPriceCalculation/CostCalculation.java
+++ src/main/java/school/productPriceCalculation/CostCalculation.java
@@ -1,32 +1,36 @@
 package school.productPriceCalculation;
 
-import school.eva.Calculation;
-
-import java.util.Arrays;
-
 import static school.eva.Calculation.calculateInterest;
 import static school.eva.Calculation.reversePercentage;
 
 public class CostCalculation {
     private double productCost = 0.00;
+    private final CostConfigInterface costConfig;
+
     public CostCalculation(CostConfigInterface costConfig){
-        this.productCost = this.calculateProductCost(costConfig);
+        this.costConfig = costConfig;
+        this.productCost = this.calculateProductCost();
     }
 
-    private double calculateProductCost(CostConfigInterface costConfig){
+    private double calculateProductCost(){
         var buyersPrice = costConfig.getBuyersPrice();
+        buyersPrice = this.substractDiscounts(buyersPrice);
+        buyersPrice = this.addCostAllocations(buyersPrice);
+        return this.addDiscounts(buyersPrice);
+    }
 
-        buyersPrice = buyersPrice - calculateInterest(buyersPrice, costConfig.getQuantityDiscount());
-        buyersPrice = buyersPrice - calculateInterest(buyersPrice, costConfig.getDeliveryDiscount());
-        buyersPrice = buyersPrice - calculateInterest(buyersPrice, costConfig.getFrequencyDiscount());
-
-        buyersPrice = buyersPrice + calculateInterest(buyersPrice, costConfig.getHoldingSurchargeAmount());
-        buyersPrice = buyersPrice + calculateInterest(buyersPrice, costConfig.getProfit());
-
-        buyersPrice = reversePercentage(buyersPrice, costConfig.getPayingDiscount());
-        buyersPrice = reversePercentage(buyersPrice, costConfig.getFrequencySellerDiscount());
-
-        return buyersPrice;
+    private double substractDiscounts(double from){
+        from = from - calculateInterest(from, this.costConfig.getQuantityDiscount());
+        from = from - calculateInterest(from, this.costConfig.getDeliveryDiscount());
+        return from - calculateInterest(from, this.costConfig.getFrequencyDiscount());
+    }
+    private double addCostAllocations(double to){
+        to = to + calculateInterest(to, costConfig.getHoldingSurchargeAmount());
+        return to + calculateInterest(to, costConfig.getProfit());
+    }
+    private double addDiscounts(double to){
+        to = reversePercentage(to, costConfig.getPayingDiscount());
+        return reversePercentage(to, costConfig.getFrequencySellerDiscount());
     }
 
     public double getProductCost(){
